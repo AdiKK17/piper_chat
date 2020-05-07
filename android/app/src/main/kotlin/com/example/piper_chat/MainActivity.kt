ee685cf01ca1af5ccfca036a455efa4475c16148
AdiKK17 <kavikmr9@gmail.com>
2020-05-07 12:21:40 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/piper_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.piper_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/piperchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.piperchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
